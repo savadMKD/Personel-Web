0e58d9d13a1dc6ed19547b9ad2eb05ef13e54213
SavadMKD <savadmoorkkanad@gmail.com>
2020-11-28 07:04:24 +0530

    Hero section and navagation bar added

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = () => {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+    var toggle = document.getElementById("toggle");
+    toggle.classList.toggle('active')
+}
